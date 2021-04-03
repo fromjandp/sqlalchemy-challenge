23df3e2ccb310baebbbd403295a0dd7d18d38e71
Judith Samuels <fromjandp@aol.com>
2021-04-03 13:25:55 -0500

    Attempt to work with precipitation

--- climate_app.py
+++ climate_app.py
@@ -1,6 +1,7 @@
-##################################
+# #######################################
 # Imports
-##################################
+# #######################################
+
 import numpy as np
 
 import sqlalchemy
@@ -10,29 +11,37 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-##################################
+# #######################################
 # Database setup
-##################################
+# #######################################
 
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-##################################
+# #######################################
 # Reflet Database into ORM classes
-##################################
+# #######################################
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Base.classes.keys()
 
-##################################
+#  print(Base.classes.keys())
+
+# Save references to the class measures
+measurement = Base.classes.measurement
+
+# Save references to the class stations
+station = Base.classes.station
+
+# ######################################
 # Flask setup
-##################################
+# #######################################
 
 app = Flask(__name__)
 
-##################################
+# #######################################
 # Flask Routes
-##################################
+# #######################################
 
 @app.route("/")
 def home_page():
@@ -44,13 +53,54 @@ def home_page():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<end><br/>"
-    )
+)
+
+#
+# preciptation api
+#
 
 @app.route("/api/v1.0/precipitation")
+def precipitation():
 
-##################################
-# Call the main app
-##################################
+    session = Session(engine)
+    results = session.query(measurement.date, measurement.prcp).all()
+
+    session.close()
+
+# Create a dictionary from the rows of data, and append to a list all_precipitation
+    all_precipitation = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"]  = precipitation
+        all_precipitation.append(precipitation_dict)
+        
+    
+    return jsonify(all_precipitation)
+#
+# stations api
+#
+
+@app.route("/api/v1.0/stations")
+def stations():
+    return (
+        f"stations path"
+    )
+
+#
+# tobs api
+#
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    return (
+        f"tobs path"
+    )
+
+# #######################################
+# Call the main Development Flask Server
+# #######################################
 
 if __name__ == "__main__":
-    app.run(debug=True)    
+        app.run(debug=True)  
+
